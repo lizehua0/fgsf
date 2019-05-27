3da722a62138cd2dc7d9342db8e990ab1f677eb1
李泽华 <lizehua@thunisoft.com>
2019-05-27 18:05:36 +0800

    *[新增test修改]

--- fgsf/src/test/test.java
+++ fgsf/src/test/test.java
@@ -16,12 +16,19 @@ public class test {
         } else {
             System.out.println("初始播撒节点写入文件失败！");
         }
-        MoveRule.DDTC.evolute(world);
+        //MoveRule.DDTC.evolute(world);
+        MoveRule.KMFG.evolute(world);
         boolean result1 = FileUtils.nodeWrite(world.getNodes(),"E:\\nodedate\\dataMoved.txt");
         if(result1){
             System.out.println("移动播撒节点写入文件成功！");
         } else {
             System.out.println("移动播撒节点写入文件失败！");
         }
+//        boolean result2 = FileUtils.writeIntoFile(world.getLineSum()/world.getDynamicNodes().size() + "","E:\\nodedate\\dataMoved.txt");
+//        if(result2){
+//            System.out.println("节点平均移动距离写入文件成功！");
+//        } else {
+//            System.out.println("节点平均移动距离写入文件失败！");
+//        }
     }
 }
